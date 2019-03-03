c725c3a96ed20f182e705ca0fdc47eb7a1b2c389
Deep <pateldeep4360@gmail.com>
2019-03-03 11:32:26 +0530

    1.0

---
+++ helpers/homepage.h
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+void Homepage()
+{
+	system("cls");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("\t # # #\t ###\t#\t#\t# # #\t ###\t# # #\n");
+	printf("\t#\t#   #\t# #\t#\t#    #\t#   #\t  #\n");
+	printf("\t#  ###\t# # #\t#  #\t#\t# # #\t# # #\t  #\n");
+	printf("\t#    #\t#   #\t#    #\t#\t#    \t#   #\t  #\n");
+	printf("\t # # #\t#   #\t#    \t#\t#    \t#   #\t  #");
+	printf("\n");
+	printf("\n");
+	printf("#    #\t#     #\t # # #\t#    #\t # # # \t # # #\t  # #\t # # #\t # # #\t #    #\n");
+	printf("#    #\t# #   #\t   #  \t#    #\t #    \t #    #\t #   \t   #  \t   #  \t  #  #\n");
+	printf("#    #\t#  #  #\t   #  \t#    #\t # # #\t # # #\t  # #  \t   #  \t   #  \t    #\n");
+	printf("#    #\t#   # #\t   #  \t #  #\t #    \t #    #\t     #\t   #  \t   #  \t   #\n");
+	printf(" #  #\t#     #\t # # #\t   #\t # # #\t #    #\t  # #\t # # #\t   #  \t  #\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("\t\t\tPress any key to continue...");
+	//getch();
+	getchar();
+}
---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "helpers/helper.h"
+#include "helpers/homepage.h"
+int main() 
+{
+   //To create home page.
+   Homepage();
+   // helper();
+   return 0;
+}
